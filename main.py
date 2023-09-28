55f559dd48bfd9c69abe3ba26dd21af3b6a20753
frank9327 <frankhadlock@icloud.com>
2023-09-28 10:30:41 -0500

    initial commint - question 1 screen, correct screen

---
+++ main.py
@@ -0,0 +1,21 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+class LoginPageApp(App):
+    pass
+
+class LoginManager(ScreenManager):
+    pass
+
+class Question1Screen(Screen):
+    pass
+
+class CorrectScreen(Screen):
+    pass
+
+
+
+
+
+LoginPageApp().run()
